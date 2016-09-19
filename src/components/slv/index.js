61c1aa6333ccad257dee7e572a20f03967d9a9b6
Keisuke Nemoto <n@on-lab.jp>
2016-09-19 11:09:29 +0900

    Oop

--- src/components/slv/index.js
+++
@@ -1,9 +0,0 @@
-
-import List from './List'
-export default List
-
-export Navi from './Navi'
-export Table from './Table'
-export TableColGroup from './TableColGroup'
-export TableHead from './TableHead'
-export TableRow from './TableRow'
